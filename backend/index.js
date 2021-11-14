b38cedc549512296b0cdb88a57bd8de6e40abf92
ryanreszetnik <rres@rogers.com>
2021-11-14 00:31:53 -0500

    format

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,9 @@ const { BigQuery } = require("@google-cloud/bigquery");
 const mysql = require("mysql");
 var app = express();
 app.get("/", async function (req, res) {
-  result = await query();
+  result = await query(
+    `SELECT * FROM \`itinerary-hackathon.itinerary_dataset.food_locations\` LIMIT 100`
+  );
   res.send(result);
 });
 const port = process.env.PORT || 8080;
@@ -12,29 +14,17 @@ app.listen(port, function () {
 });
 
 const bigquery = new BigQuery();
-async function query() {
-  // Queries the U.S. given names dataset for the state of Texas.
-
-  const query = `SELECT *
-    FROM \`itinerary-hackathon.itinerary_dataset.food_locations\`
-    LIMIT 100`;
-
-  // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+async function query(query) {
+  // const query = `SELECT *
+  //   FROM \`itinerary-hackathon.itinerary_dataset.food_locations\`
+  //   LIMIT 100`;
   const options = {
     query: query,
-    // Location must match that of the dataset(s) referenced in the query.
     location: "northamerica-northeast1",
   };
 
-  // Run the query as a job
   const [job] = await bigquery.createQueryJob(options);
-  console.log(`Job ${job.id} started.`);
 
-  // Wait for the query to finish
   const [rows] = await job.getQueryResults();
-
-  // Print the results
-  console.log("Rows:");
-  rows.forEach((row) => console.log(row));
   return rows;
 }
